891010e342c662492dc0ffe9744d1198f08ac3ff
LemmyMG <lemmymgee@gmail.com>
2026-08-21 11:54:02 +0100

    Get user guess

---
+++ Project_2_Number_Guessing_Game/Number_Guessing_Game_Stage_2.py
@@ -0,0 +1,5 @@
+#Store the secret number
+secret_number = 7
+
+# Ask the user for a number, convert their answer into an integer, and store that integer in guess
+secret_number = int(input("Guess the number"))
